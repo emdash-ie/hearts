5b015e3dc65d4f5d689045427b334e152f287c74
Emily Bourke <undergroundquizscene@protonmail.com>
2021-08-18 23:23:54 +0100

    Use shell.nix for haskell-language-server
    (Since it’s not actually a build dependency!)

--- shell.nix
+++ shell.nix
@@ -1,6 +1,8 @@
 let
   pkgs = import <nixpkgs> { };
 in pkgs.mkShell {
-  packages = [ pkgs.haskellPackages.fourmolu ];
+  packages = [ pkgs.haskellPackages.fourmolu
+               pkgs.haskell-language-server
+             ];
   inputsFrom = [ (import ./release.nix).hearts.env ];
 }
